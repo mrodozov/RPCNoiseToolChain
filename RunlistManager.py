c39f3f416154c18658d8f7f71db4a9100e2709ba
rodozov <mrodozov@gmail.com>
2015-10-14 08:21:57 +0300

    Processed runs queue updated

--- RunlistManager.py
+++ RunlistManager.py
@@ -6,7 +6,6 @@ import time
 import socks
 from threading import Thread, Lock, Event
 import Queue
-from DBService import DBService
 
 # TODO - Figure it how to synch changes from the remote runlist. Another thread in runlist is required to 1. Keep time of last modification on the remote rl, and verify it. if changed, get, remote rl file
 
@@ -16,15 +15,12 @@ class RunlistManager(Thread):
     run list manager manages
     1. file with runs
     2. new runs using the RRApi
-    3. changes
+    3. changes in the remote location runlist (resubmit queries)
     '''
 
     def __init__(self, runlist=None):
 
-        '''
-        :param runlist: run list is file with json description of runs analyzed.
-        :return: none
-        '''
+
         super(RunlistManager, self).__init__()
         self.runlist = {} #lock when modify the list
         self.rr_connector = None
@@ -39,10 +35,8 @@ class RunlistManager(Thread):
         self.loadRunlistFile(runlist)
         self.runlistLock = Lock()
 
-
-
     def __del__(self):
-        #do some shit
+        # make sure clear all the queues, stop threads and delete them
         return None
 
     def getFirstLastRuns(self):
@@ -82,15 +76,8 @@ class RunlistManager(Thread):
         return retval
 
     def updateRunlistFile(self,runlistFile,oldFile=None):
-        '''
-        writes the runlist file using the merged runlist object. careful with this method ! it writes the file with runs, so if the runlist is emptied, it may wipe out the history !
-        :param runlistFile: runlist file
-        :param oldFile: previous version of the file, backup
-        :return: success
-        '''
-        retval = False
-        #TODO - implement protection, like copying the file opened with another name and then writing. UPDATE - implement archive, not protection
 
+        #TODO - implement protection, like copying the file opened with another name and then writing. UPDATE - implement archive, not protection
 
         with open(runlistFile,"w") as runFile:
             runFile.write(json.dumps(self.runlist, indent=1, sort_keys=True))
@@ -101,18 +88,12 @@ class RunlistManager(Thread):
 
     def getListOfRunsToProcess(self):
 
-        '''
-        get only the runs to be processed
-        :return: return only runs to be processed from all, as dictionary of run{}
-        '''
         shortlist = {}
-        self.runlistLock.acquire()
-        for run in self.runlist:
-            stat = self.runlist[run]['status']
-            if stat != 'finished' and stat != 'submitted':
-                shortlist[run] = self.runlist[run]
-        self.runlistLock.release()
-
+        with self.runlistLock:
+            for run in self.runlist:
+                stat = self.runlist[run]['status']
+                if stat != 'finished' and stat != 'submitted' and stat != 'Failed':
+                    shortlist[run] = self.runlist[run]
         return shortlist
 
     def putRunsOnProcessQueue(self, runlist = None):
@@ -136,35 +117,26 @@ class RunlistManager(Thread):
             rnum = None
             run_details = {}
             run_results = None
-            run_warnings = None
-            run_logs = None
             for r in run.keys():
                 rnum = r
             run_details = run[rnum]
             print 'Run ', rnum, ' in results'
+            run_status = 'finished'
             try:
                 print run_details.keys()
                 run_results = run_details['results']
-                run_warnings = run_details['warnings']
-                run_logs = run_details['logs']
-            except KeyError, e:
-                e.message
 
-            #print run_results
-            # check the status
-            run_status = 'finished'
-            try:
                 for k in run_results:
                     run_status = 'finished'
                     if run_results[k][0] == 'Failed':
                         run_status = 'Failed'
-                        self.reportQueue.put({rnum:{"warnings":run_warnings, "logs":run_logs}})
+                        self.reportQueue.put(run)
                         break
-            except Exception, e:
+            except KeyError, e:
                 e.message
             #update runlist
-            self.updateRun(rnum,'status',run_status)
-            self.processedRunsQueue.task_done() #
+            self.updateRun(rnum, 'status', run_status)
+            self.processedRunsQueue.task_done()
 
             if self.stop_event.is_set() and self.processedRunsQueue.empty():
                 # do some finishing stuff
@@ -194,10 +166,7 @@ class RunlistManager(Thread):
             if new_runs : current_sleep_time = init_sleep_time # reset when new runs arrived
             current_sleep_time += init_sleep_time # increments every time with one minute if RR does not return new runs
             # if new runs are available, put them on the runlist
-            #if not 'new' or 'submitted' in self.runlist.keys(): continue
-
             self.addruns(new_runs)
-
             # get runs to process
             list_to_process = self.getListOfRunsToProcess()
             # order them and put on queue
@@ -206,13 +175,15 @@ class RunlistManager(Thread):
 
             for r in ordered_list_to_process.keys():
 
-                self.updateRun(r,'status','submitted')
-                print self.runlist
+                self.updateRun(r, 'status', 'submitted')
+                self.toProcessQueue.put({r: self.runlist[r]})
+
+            #if new_runs : print ordered_list_to_process.keys()
+            print self.runlist
 
             print 'go to sleep for', current_sleep_time , 'seconds'
-            time.sleep(current_sleep_time) # increase this and
-
             print 'RR checked last run ', last_run
+            time.sleep(current_sleep_time)
 
             if self.stop_event.is_set() and self.processedRunsQueue.empty():
                 print 'RR exits'
@@ -230,10 +201,13 @@ class RunlistManager(Thread):
 def moveQueueEntries(inputQueue = None, outputQueue = None):
     while True:
         input = inputQueue.get()
-        #input['results'] =
-        outputQueue.put(input)
+        sumdict = input.copy()
+        rnum = [r for r in input.keys()][0]
+        sumdict[rnum]['results'] = {'test':['finished'], 'sectest': ['Failed']}
+        sumdict[rnum]['warnings'] = {'test':['finished'], 'sectest': ['Failed']}
+        sumdict[rnum]['logs'] = {'test':['finished'], 'sectest': ['Failed']}
+        outputQueue.put(sumdict)
         inputQueue.task_done()
-
 
 if __name__ == "__main__":
 
@@ -242,7 +216,6 @@ if __name__ == "__main__":
     reportsQueue = Queue.Queue()
     stop_rlistmngr = Event()  # set this to kill the loop and exit
     mq = Thread(target=moveQueueEntries, args=(runsToProcessQueue, processedRunsQueue))
-
 
     socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, '127.0.0.1', 1080)
     rr_obj = RRService(use_proxy=True)
@@ -253,8 +226,8 @@ if __name__ == "__main__":
     rlistMngr.reportQueue = reportsQueue
     rlistMngr.rr_connector = rr_obj
     rlistMngr.stop_event = stop_rlistmngr
-
     rlistMngr.check_run_registry.start()
+
     delay = 10
     while delay != 0:
         time.sleep(1)
@@ -272,22 +245,5 @@ if __name__ == "__main__":
 
     rlistMngr.check_completed_runs.start()
 
-
-
-    #rlist = rr_obj.getRunRange('Collisions15', '257000', '600')
-    #print rlist
-    #rlist = rr_obj.getRunlistForLongRuns(258000)
-    #print rlist
-    #lumis = rr_obj.getRunsLumiSectionsInfo(lastRun=257000)
-    #print lumis
-    #print rlistMngr.runlist.keys()
-    #res = rlistMngr.rr_connector.getRunRangeWithLumiInfo('Collisions15 OR Cosmics15 ', '258712', '600')
-
-    #print res
-    #res_sorted = res.keys()
-    #res_sorted.sort()
-    #print res_sorted
-    #rlistMngr.runlist['258713'] = res['258713']
-    #rlistMngr.updateRunlistFile(rlistFile)
 
 
